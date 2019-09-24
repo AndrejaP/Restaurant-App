28acd2382e472cb7601f08788d00bf7db41e85af
Andreja <andreja.prpic@gmail.com>
2019-09-24 14:50:08 +0200

    feat: Get the map on the screen with Mapbox key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYW5kcmVqYXAiLCJhIjoiY2swdnpsanNqMHd6ZjNkcXB6dDJoN2FiNCJ9.wiLcsfzn6etUQdKjLw8Jtg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
