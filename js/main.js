c0213e7dcb7ae7e170036e328dda762e03f20981
Andreja <andreja.prpic@gmail.com>
2019-09-30 17:30:35 +0200

    feat: Register Service Worker

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ let restaurants,
 var newMap
 var markers = []
 
+//registering sw
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker
+          .register('../sw_cached_pages.js')
+          .then(reg => console.log('Service Worker: Registered'))
+          .catch(err => console.log(`Service Worker: Error: ${err}`));
+  });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
