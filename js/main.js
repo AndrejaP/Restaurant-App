9711c0da12d46fb29184fbbe12bf43679a4c7360
Andreja <andreja.prpic@gmail.com>
2019-10-01 18:27:09 +0200

    feat: Installation and activation of Service Worker
    Start adjusting the css for mobile devices

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ let restaurants,
 var newMap
 var markers = []
 
-//registering sw
-if('serviceWorker' in navigator) {
+//registering Service Worker
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker
-          .register('../sw_cached_pages.js')
-          .then(reg => console.log('Service Worker: Registered'))
-          .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
+    navigator.serviceWorker
+      .register('/sw_cached_site.js')
+      // Registration was successful
+      .then(registration => console.log(`Service Worker: Registered with scope: ${registration.scope}`))
+      // registration failed 
+      .catch(err => console.log(`Service Worker: Error: ${err}`));
+    });
 }
 
 /**
